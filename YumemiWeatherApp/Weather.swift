b59f1ea99ddc81868489eeb01733c8d79f53dd40
Takeya Shimizu <takeya.shimizu@sportsbull.jp>
2022-02-12 11:58:09 +0900

    Add: Weather struct

---
+++ YumemiWeatherApp/Weather.swift
@@ -0,0 +1,15 @@
+//
+//  Weather.swift
+//  YumemiWeatherApp
+//
+//  Created by Takeya Shimizu on 2022/02/12.
+//
+
+import Foundation
+
+/// 天気予報データ
+struct WeatherReport {
+    let weather: Weather
+    let maxTemp: Int
+    let minTemp: Int
+}

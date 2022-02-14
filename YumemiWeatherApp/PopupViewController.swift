9115b9aad0fff96fbf92d099f59e4591722cc937
Takeya Shimizu <takeya.shimizu@sportsbull.jp>
2022-02-14 15:07:55 +0900

    Update: VCのライフサイクルメソッドのログメッセージを修正 (確認用)

--- YumemiWeatherApp/PopupViewController.swift
+++ YumemiWeatherApp/PopupViewController.swift
@@ -12,34 +12,34 @@ class PopupViewController: UIViewController {
     // MARK: Lifecycle
 
     override func loadView() {
-        print("[lifecycle]", #function)
+        print("[lifecycle (VC)]", #function)
         super.loadView()
     }
 
     override func viewDidLoad() {
-        print("[lifecycle]", #function)
+        print("[lifecycle (VC)]", #function)
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        print("[lifecycle]", #function)
+        print("[lifecycle (VC)]", #function)
         super.viewWillAppear(animated)
     }
 
     override func viewWillLayoutSubviews() {
-        print("[lifecycle]", #function)
+        print("[lifecycle (VC)]", #function)
         super.viewWillLayoutSubviews()
     }
 
     override func viewDidLayoutSubviews() {
-        print("[lifecycle]", #function)
+        print("[lifecycle (VC)]", #function)
         super.viewDidLayoutSubviews()
     }
 
     override func viewDidAppear(_ animated: Bool) {
-        print("[lifecycle]", #function)
+        print("[lifecycle (VC)]", #function)
         super.viewDidAppear(animated)
 
         //ViewController に遷移
@@ -49,12 +49,12 @@ class PopupViewController: UIViewController {
     }
 
     override func viewWillDisappear(_ animated: Bool) {
-        print("[lifecycle]", #function)
+        print("[lifecycle (VC)]", #function)
         super.viewWillDisappear(animated)
     }
 
     override func viewDidDisappear(_ animated: Bool) {
-        print("[lifecycle]", #function)
+        print("[lifecycle (VC)]", #function)
         super.viewDidDisappear(animated)
     }
 
